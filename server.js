19e7de0812b0f149ed9c1b45aef0935a35ada939
Camburger9 <camburger9@gmail.com>
2025-09-18 20:22:08 +0000

    finished up assignment

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-//call back function
-serveStatic = function (req, res) {
-	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
+const path = require('path');
+
+function serveStatic(req, res) {
+    let fileName = '.' + url.parse(req.url).pathname;
+    let ext = path.extname(fileName);
+
+    fs.readFile(fileName, function(err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('404 Not Found\n');
+        } else if (ext === '.txt') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+        } else if (ext === '.jpg') {
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(data);
+        }
+    });
 }
- 
-const myserver = http.createServer(serveStatic); //create a server object
-myserver.listen(80, function() {console.log("Listening on port 80" )}); 
+
+const myserver = http.createServer(serveStatic);
+myserver.listen(80, function () {
+    console.log("Listening on port 80");
+});
 
